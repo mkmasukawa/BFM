b217f24e16e5970ccb23e1fa4bdd9310003e42e2
Marcos Masukawa <masukawa.m.aa@m.titech.ac.jp>
2020-06-09 23:03:21 +0900

    Commit test

--- BFM_3D.py
+++ BFM_3D.py
@@ -6,7 +6,7 @@ m0 = 4; m1 = 3; m2 = 2; m3 = 3; m4 = 6; m5 = 2; m6 = 6; m7 = 4;
 r = sin(m0*phi)**m1 + cos(m2*phi)**m3 + sin(m4*theta)**m5 + cos(m6*theta)**m7
 x = r*sin(phi)*cos(theta)
 y = r*cos(phi)
-z = 4*2*r*sin(phi)*sin(theta)
+z = 1.5*2*r*sin(phi)*sin(theta)
 
 
 # View it.
