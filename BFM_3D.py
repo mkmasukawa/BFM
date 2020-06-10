87f5d68f140a68abe1e6fc025077befd7fa06575
Marcos Masukawa <masukawa.m.aa@m.titech.ac.jp>
2020-06-10 21:33:55 +0900

    Commit test

--- BFM_3D.py
+++ BFM_3D.py
@@ -1,16 +1,46 @@
-from numpy import pi, sin, cos, mgrid
+import numpy as np
+from mayavi import mlab
+import itertools
 
-dphi, dtheta = pi/250.0, pi/250.0
-[phi,theta] = mgrid[0:pi+dphi*1.5:dphi,0:2*pi+dtheta*1.5:dtheta]
-m0 = 4; m1 = 3; m2 = 2; m3 = 3; m4 = 6; m5 = 2; m6 = 6; m7 = 4;
-r = sin(m0*phi)**m1 + cos(m2*phi)**m3 + sin(m4*theta)**m5 + cos(m6*theta)**m7
-x = r*sin(phi)*cos(theta)
-y = r*cos(phi)
-z = 4*2*r*sin(phi)*sin(theta)
-a = 1
+# generates all possible bonds
+
 
 # View it.
-from mayavi import mlab
-s = mlab.mesh(x, y, z)
-mlab.show()
+# from mayavi import mlab
+# s = mlab.mesh(x, y, z)
+# mlab.show()
 
+vector_list = [[2, 0, 0], [2, 1, 0], [2, 1, 1], [2, 2, 1], [3, 0, 0], [3, 1, 0]]
+signal_permutation_list = [[1, 1, 1], [1, -1, -1], [1, 1, -1], [1, -1, 1], [-1, 1, 1], [-1, -1, -1], [-1, 1, -1],
+                           [-1, -1, 1]]
+complete_vector_list = []
+
+for i in vector_list:
+    for j in list(itertools.permutations(i)):
+        for k in signal_permutation_list:
+            complete_vector_list.append([a * b for a, b in zip(j, k)])
+
+vector_list_no_duplicate = []
+
+for i in complete_vector_list:
+    if i not in vector_list_no_duplicate:
+        vector_list_no_duplicate.append(i)
+
+vector_list_no_duplicate = np.array(vector_list_no_duplicate)
+
+def test_quiver3d(vec):
+    n = len(vec)
+    print(n)
+    x = np.zeros(n)
+    y = np.zeros(n)
+    z = np.zeros(n)
+    u, v, w = vec.T
+
+    obj = mlab.quiver3d(x, y, z, u, v, w, line_width=3, scale_factor=1)
+    return obj
+
+
+if __name__ == '__main__':
+    test_quiver3d(vector_list_no_duplicate)
+    mlab.show()
+    print(len(vector_list_no_duplicate))
