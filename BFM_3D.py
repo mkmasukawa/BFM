58467424de432cd32331cf5ca1447c87422dd73e
Marcos Masukawa <masukawa.m.aa@m.titech.ac.jp>
2020-06-22 15:19:19 +0900

    Added function that adds motifs, started writing the function that updates position

--- BFM_3D.py
+++ BFM_3D.py
@@ -7,9 +7,13 @@ import bfm
 
 if __name__ == '__main__':
 
-    box_dimension = 10
-    particle_number = 1
+    box_dimension = 128
+    particle_number = 400
+    MCS_steps = 10
     #creates the box and places the particles in ordered position
     molecules_matrix = bfm.creates_box(box_dimension, particle_number)
+
+    for i in range(MCS_steps):
+        bfm.update_system(molecules_matrix)
 
     bfm.plots_bonds(molecules_matrix)
--- bfm.py
+++ bfm.py
@@ -1,6 +1,7 @@
 import numpy as np
 import itertools
 from mayavi import mlab
+import random
 
 def generates_vectors():
     vector_list = [[2, 0, 0], [2, 1, 0], [2, 1, 1], [2, 2, 1], [3, 0, 0], [3, 1, 0]]
@@ -22,6 +23,10 @@ def generates_vectors():
     return vector_list_no_duplicate
 
 VECTORS = generates_vectors()
+
+DICT_INT_TO_VECTOR = dict(zip(range(len(VECTORS)), [tuple(l) for l in VECTORS]))
+
+DICT_VECTOR_TO_INT = {v: k for k, v in DICT_INT_TO_VECTOR.items()}
 
 #https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
 def chunks(lst, n):
@@ -49,12 +54,12 @@ def plots_bonds(bond_matrix):
                 b = [int(x) for x in str(a)]
                 split_bonds = chunks(b[:-1], 4)
                 for bond in split_bonds:
-                    connections.append([(i, j, k), (i+bond[1], j+bond[2], k+bond[3])])
-                    # x.append((i, j, k))
-                    # x.append((i+bond[1], j+bond[2], k+bond[3]))
-                    # x.append((i+bond[1], j+bond[2], k+bond[3]))
-                    # connections.append([i*x_len + x_len + j*y_len + k*z_len, (i+bond[1])*x_len + (j+bond[2])*y_len + (k+bond[3])*z_len])
-
+                    # connections.append([(i, j, k), (i+bond[1], j+bond[2], k+bond[3])])
+                    strings = [str(integer) for integer in bond[1:]]
+                    a_string = "".join(strings)
+                    an_integer = int(a_string)
+                    bond_coordinates = DICT_INT_TO_VECTOR[an_integer]
+                    connections.append([(i, j, k), (i+bond_coordinates[0], j+bond_coordinates[1], k+bond_coordinates[2])])
 
     connection_index = []
     for c in connections:
@@ -63,45 +68,86 @@ def plots_bonds(bond_matrix):
     u, v, w = np.array(x).T
     scalars = np.array(scalars)
 
-    # connections = [(1, 2), (3, 4)]
-
-    # for start, stop in edges:
-    #     connections.append((labels[start], labels[stop]))
-    # mlab.figure(1, bgcolor=(0, 0, 0))
     mlab.clf()
 
-    pts = mlab.points3d(u*10, v*10, w*10, 1.5 * scalars.max() - scalars,
+    pts = mlab.points3d(u*10, v*10, w*10, 2 * scalars.max() - scalars,
                         scale_factor=10, resolution=7)
     pts.mlab_source.dataset.lines = np.array(connection_index)
-
-    # mlab.plot3d([1,2,3, 1], [2,3,4, 2], [5,6,7, 1], [1, 2, 1, 3],
-    #             tube_radius=0.1, colormap='Reds', tube_sides = 6)
-
-    #Use a tube fiter to plot tubes on the link, varying the radius with the
-   # scalar value
 
     tube = mlab.pipeline.tube(pts, tube_radius=0.0001, tube_sides=4)
     tube.filter.radius_factor = 1
     tube.filter.vary_radius = 'vary_radius_by_scalar'
     mlab.pipeline.surface(tube, color=(0.8, 0.8, 0))
 
-    # Visualize the local atomic density
-    # mlab.pipeline.volume(mlab.pipeline.gaussian_splatter(pts))
-
-
     mlab.show()
 
-    #obj = mlab.quiver3d(x, y, z, u, v, w, line_width=3, scale_factor=1)
-    #mlab.show()
-    # return obj
 
+def update_system(bond_matrix):
+    x = list(range(len(bond_matrix)))
+    y = list(range(len(bond_matrix[0])))
+    z = list(range(len(bond_matrix[0][0])))
+    x_random = random.choice(x)
+    y_random = random.choice(y)
+    z_random = random.choice(z)
+    for i in range(200):
+        if bond_matrix[x_random, y_random, z_random] != 0:
+            break
+        else:
+            x_random = random.choice(x)
+            y_random = random.choice(y)
+            z_random = random.choice(z)
+    update_position(bond_matrix, x_random, y_random, z_random)
 
+def update_position(bond_matrix, i, j, k):
+    if bond_matrix[i, j, k] != 0:
+        # choose random vector
+        available_positions = []
+        #see which of the movements are available
+        for v in VECTORS:
+            if bond_matrix[i+v[0], j+v[1], k+v[2]] == 0:
+                available_positions.append(v)
+        chosen_v = random.choice(available_positions)
+        #check if the position is available
+        #if it is available remake the bonds
+        print(available_positions)
+    else:
+        print("value is zero")
 
 
 def creates_box(M, N):
     test_m = np.zeros((M, M, M), dtype=int)
-    test_m[3, 3, 3] = 12001
-    test_m[5, 3, 3] = 1
+
+    def particle(i, j , k):
+        # if 10 < i < M and 10 < j < M and 10 < k < M:
+        if 6 < i < M-6 and 6 < j < M-6 and 6 < k < M-6:
+            test_m[i, j, k] = 1002106110601
+            test_m[i - 2, j + 2, k - 1] = 105710601
+            test_m[i - 4, j + 4, k - 2] = 105710601
+            test_m[i - 6, j + 6, k - 3] = 10571
+            test_m[i - 2, j - 2, k + 1] = 105610611
+            test_m[i - 4, j - 4, k + 2] = 105610611
+            test_m[i - 6, j - 6, k + 3] = 10561
+            test_m[i, j + 2, k] = 100310021
+            test_m[i, j + 4, k] = 100310021
+            test_m[i, j + 6, k] = 1
+        # else:
+        #     raise Exception("Values outside matrix")
+
+    x = list(range(len(test_m)))
+    y = list(range(len(test_m[0])))
+    z = list(range(len(test_m[0][0])))
+
+    for i in range(N):
+
+        x_random = random.choice(x)
+        y_random = random.choice(y)
+        z_random = random.choice(z)
+        particle(x_random, y_random, z_random)
+    # print(DICT_VECTOR_TO_INT)
+    # print(DICT_VECTOR_TO_INT[(0, -2, 0)])
+
+    # test_m[3, 3, 3] = 12001
+    # test_m[5, 3, 3] = 1
     return test_m
 
     # for i in range(M):
