b2e729246a0d51f36604ac178c057d1647db7760
Marcos Masukawa <masukawa.m.aa@m.titech.ac.jp>
2020-06-26 19:09:25 +0900

    Added the functions that connects the sticky ends (but not yet the one that breaks them)

--- bfm.py
+++ bfm.py
@@ -47,9 +47,6 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 
-# def vector_to_integer(vector):
-
-
 # the information of the bonds is stored in the matrix values in the form of an int
 # this returns a dictionary
 def split_info(a, i, j, k):
@@ -100,18 +97,7 @@ def particle_coordinates(bond_matrix):
         scalars = np.array(scalars)
         return u, v, w, scalars, connection_index
 
-#
-# class Wrap:
-#
-#     def __init__(self, val):
-#         self.val = val
-#
-#     def unlink(self):
-#         val = self.val
-#         self.val = None
-#         return val
-
-def animates(bond_matrix):
+def animates(bond_matrix, max_steps):
     # View it.
     x, y, z, scalars, connections = particle_coordinates(bond_matrix)
     l = mlab.points3d(10*x, 10*y, 10*z, scale_factor=5)
@@ -131,14 +117,15 @@ def animates(bond_matrix):
         # for i in range(duration):
         i = 0
         # while(True):
-        while(i < 10000):
+        while(i < max_steps):
             print(i)
             x, y , z, s, c = particle_coordinates(bond_matrix)
             # chunk = Wrap(ms.set(x=10*x, y=10*y, z=10*z))
             ms.set(x=10 * x, y=10 * y, z=10 * z)
             # f.scene.render()
 
-            update_system(bond_matrix)
+            # update_system(bond_matrix)
+            update_system_with_bonds(bond_matrix)
             i += 1
             # yield chunk.unlink()
             yield
@@ -153,18 +140,29 @@ def plots_bonds(bond_matrix):
     z_len = len(bond_matrix[0][0])
     scalars, connections = [], []
     x = []
+    colors = []
     for i in range(x_len):
         for j in range(y_len):
             for k in range(z_len):
-                a = bond_matrix[i][j][k]
-                if a != 0:
-                    m_d = split_info(a, i, j, k)
+                cell = bond_matrix[i][j][k]
+                if cell != 0:
+                    m_d = split_info(cell, i, j, k)
+                    monomer_type = m_d["type"]
                     m_d.pop("type")
                     for m in m_d:
                         # if (i, j, k) not in x:
                         x.append((i, j, k))
                         x.append((i + m[0], j + m[1], k + m[2]))
-                        scalars.append(1)
+                        # scalars.append(1)
+                        if monomer_type == 1:
+                            # colors.append((1, 0, 0))
+                            scalars.append(0.25)
+                        if monomer_type == 2:
+                            # colors.append((0, 1, 0))
+                            scalars.append(0.5)
+                        if monomer_type == 3:
+                            scalars.append(0.75)
+                            # colors.append((0, 0, 1))
                         scalars.append(1)
 
     if x:
@@ -178,8 +176,12 @@ def plots_bonds(bond_matrix):
         u, v, w = np.array(x).T
         scalars = np.array(scalars)
 
+        # pts = mlab.points3d(u * 10, v * 10, w * 10, 2 * scalars.max() - scalars,
+        #                     scale_factor=10,
+        #                     resolution=7, color=colors)
+
         pts = mlab.points3d(u * 10, v * 10, w * 10, 2 * scalars.max() - scalars,
-                            scale_factor=10, resolution=7)
+                            scale_factor=10, resolution=7, colormap="copper")
 
         pts.mlab_source.dataset.lines = np.array(connection_index)
         tube = mlab.pipeline.tube(pts, tube_radius=0.0001, tube_sides=4)
@@ -188,50 +190,170 @@ def plots_bonds(bond_matrix):
         mlab.pipeline.surface(tube, color=(0.8, 0.8, 0))
 
         # print(type(mlab.screenshot()))
-        # mlab.show()
+        mlab.show()
         # return mlab.screenshot()
-        # mlab.savefig(str(number) + '.png')
+        # mlab.savefig("equilibrated" + '.png')
         # mlab.savefig("test.vtk")
-        pts.module_manager.source.save_output('output.vtk')
+        # pts.module_manager.source.save_output('output.vtk')
 
-# def make_frame(bond_matrix):
-#     update_system(bond_matrix)
-#     arr = plots_bonds(bond_matrix)
-#     pl.imshow(arr)
-#     pl.show()
+
 #TODO: make simpified plot of the whole system if too heavy to be loaded by mayavi
-
 
 
 
 # updates the system within a given spot
 # because it looks only at a slice of the matrix, this is why the coordinates need to be relative
 def update_system(bond_matrix):
-    # bond_matrix_copy = bond_matrix
-    x = list(range(len(bond_matrix)))
-    y = list(range(len(bond_matrix[0])))
-    z = list(range(len(bond_matrix[0][0])))
-    x_random = random.choice(x)
-    y_random = random.choice(y)
-    z_random = random.choice(z)
-    # tries to find a spot that has a monomer
-
     filled_cells = np.nonzero(bond_matrix)
     if filled_cells:
         chosen = random.choice(np.transpose(filled_cells))
         update_position(bond_matrix, chosen[0], chosen[1], chosen[2])
     return bond_matrix
 
-    # for i in range(200):
-    #     print("trying to find")
-    #     if bond_matrix[x_random, y_random, z_random] != 0:
-    #         break
-    #     else:
-    #         x_random = random.choice(x)
-    #         y_random = random.choice(y)
-    #         z_random = random.choice(z)
-    # update_position(bond_matrix, x_random, y_random, z_random)
 
+def update_positions_bonds(bond_matrix, i, j, k):
+    # only positions that have monomers can be modified
+    if bond_matrix[i, j, k] != 0:
+        # see how many positions are allowed
+        move_to = chooses_allowed_movement(bond_matrix, i, j, k)
+        if move_to:
+            # check all the atoms that are connected to the bonds and modify them too
+
+            # change the atom first
+            old_mon_dict = split_info(bond_matrix[i, j, k], i, j, k)
+            new_mon = str(old_mon_dict["type"])
+            old_mon_dict.pop("type")
+            for m in old_mon_dict:
+                # redo the bond in the monomer
+                new_bond_int = DICT_VECTOR_TO_INT[(m[0] - move_to[0], m[1] - move_to[1], m[2] - move_to[2])]
+                new_mon = str(old_mon_dict[m]) + str(new_bond_int).zfill(3) + new_mon
+                # redo the bonds in the neighbors
+                # neighbor bonds
+                # print(bond_matrix[i + m[0], j + m[1], k + m[2]])
+                neighbor_dict = split_info(bond_matrix[i + m[0], j + m[1], k + m[2]], i + m[0], j + m[1], k + m[2])
+                # print("neighbour_dict", neighbor_dict)
+                to_be_changed = (-m[0], -m[1], -m[2])
+                neighbor_dict[(-m[0] + move_to[0], -m[1] + move_to[1], -m[2] + move_to[2])] = old_mon_dict[m]
+                del neighbor_dict[to_be_changed]
+                # print(neighbor_dict)
+                c_m = str(neighbor_dict["type"])
+                del neighbor_dict["type"]
+                # join the bonds again
+                for n in neighbor_dict:
+                    c_m = str(neighbor_dict[n]) + str(DICT_VECTOR_TO_INT[n]).zfill(3) + c_m
+                # convert dict back to int
+                bond_matrix[i + m[0], j + m[1], k + m[2]] = int(c_m)
+
+            bond_matrix[i, j, k] = 0
+            bond_matrix[i + move_to[0], j + move_to[1], k + move_to[2]] = new_mon
+
+            #after moving the atom, check if it is close to any monomer which is a type 2
+            #forming bond
+            #if two type 2 are nearby, then the types 3 they are attached to are removed and they are connected,
+            # remember to do the bond both ways
+            new_i = i + move_to[0]
+            new_j = j + move_to[1]
+            new_k = k + move_to[2]
+            current_mon = split_info(bond_matrix[new_i, new_j, new_k],  new_i, new_j, new_k )
+            if current_mon["type"] == 2:
+                del current_mon["type"]
+                #check if it is bonded to type 2 already
+                #calculate here probability to break
+                bonded_to_type_2 = False
+                for b in current_mon:
+                    bonded_to_i = new_i + b[0]
+                    bonded_to_j = new_j + b[1]
+                    bonded_to_k = new_k + b[2]
+                    bonded_to = split_info(bond_matrix[bonded_to_i, bonded_to_j, bonded_to_k], bonded_to_i,
+                                           bonded_to_j, bonded_to_k)
+                    if bonded_to["type"] == 2:
+                        bonded_to_type_2 = True
+
+
+                if bonded_to_type_2 == True:
+                    print("already bonded to type 2")
+                    # calculate probability to break
+                #not bonded to type 2 yet
+                else:
+                    #calculate probability to bond
+                    #check is there is a type 2 nearby
+                    neighbors_type2 = []
+                    for v in VECTORS:
+                        neighbor_i = new_i + v[0]
+                        neighbor_j = new_j + v[1]
+                        neighbor_k = new_k + v[2]
+                        if 0 <= neighbor_i < len(bond_matrix) and 0 <= neighbor_j < len(bond_matrix[0]) and \
+                                0 <= neighbor_k < len(bond_matrix[0][0]):
+                            neighbor_mon = split_info(bond_matrix[neighbor_i, neighbor_j, neighbor_k], neighbor_i,
+                                                      neighbor_j, neighbor_k)
+                            if neighbor_mon["type"] == 2:
+                                neighbors_type2.append((neighbor_i, neighbor_j, neighbor_k))
+                                print("there is neighbor type 2")
+                            # if neighbor_mon["type"] == 3:
+                            #     neighbors_type3.append((neighbor_i, neighbor_j, neighbor_k))
+                            #     print("there is neighbor type 3")
+                            #make a bond
+                            #maybe there is more than one neighbor type 2, in this case one is randomly chosen
+                    #there are neigbors type 2 that can be bonded to
+                    if neighbors_type2:
+                        chosen_type2 = random.choice(neighbors_type2)
+                        #remove the monomers type 3 connected to
+
+                        print(DICT_VECTOR_TO_INT)
+
+                        #save from dict to the matrix
+                        bonded_current = str(3) + str(DICT_VECTOR_TO_INT[(chosen_type2[0] - new_i,
+                                                                          chosen_type2[1] - new_j,
+                                                                          chosen_type2[2] - new_k)] )+ str(2)
+                        for c_m in current_mon:
+                            if str(bond_matrix[new_i+c_m[0], new_j+c_m[1], new_k+c_m[2]])[-1] == "3":
+                                bond_matrix[new_i + c_m[0], new_j + c_m[1], new_k + c_m[2]] = 0
+                            else:
+                                bonded_current = str(current_mon[c_m]) + str(DICT_VECTOR_TO_INT[c_m]).zfill(3) \
+                                                 + bonded_current
+                                #remove also from the perspective of type 3
+
+                        # print(bonded_current)
+                        bond_matrix[new_i, new_j, new_k] = int(bonded_current)
+                        bond_to_dict = split_info(bond_matrix[chosen_type2[0], chosen_type2[1],
+                                                              chosen_type2[2]], chosen_type2[0],
+                                                  chosen_type2[1], chosen_type2[2])
+                        del bond_to_dict["type"]
+                        bonded_neighbor = str(3) + str(DICT_VECTOR_TO_INT[(-chosen_type2[0] + new_i, -chosen_type2[1] + new_j, -chosen_type2[2] + new_k)]) + str(2)
+                        for d in bond_to_dict:
+                            # print(chosen_type2, d)
+                            if str(bond_matrix[chosen_type2[0]+d[0], chosen_type2[1]+d[1], chosen_type2[2]+d[2]])[-1] == "3":
+                                bond_matrix[chosen_type2[0] + d[0], chosen_type2[1] + d[1], chosen_type2[2] + d[2]] = 0
+                            else:
+                                bonded_neighbor = str(bond_to_dict[d]) + str(DICT_VECTOR_TO_INT[d]).zfill(3) + bonded_neighbor
+                        # print(bonded_neighbor)
+                        #save dict to the matrix
+                        bond_matrix[chosen_type2] = int(bonded_neighbor)
+                        #delete the other monomers
+
+
+                                #break bonds to type 3 of the types 2 that were bonded
+
+                #breaking bond
+                #if a bond is broken between 2 type 2, check if there is space for creating type 3 monomers that c
+                # an be connected to them
+                #is yes, then break the bond, create the new monomers type 3 and bind them to the respective type 3,
+                # remember to do the bond both ways
+
+
+            return True
+        else:
+            return False
+    else:
+        return False
+
+#updates system for moving monomers but also bond formation
+def update_system_with_bonds(bond_matrix):
+    filled_cells = np.nonzero(bond_matrix)
+    if filled_cells:
+        chosen = random.choice(np.transpose(filled_cells))
+        update_positions_bonds(bond_matrix, chosen[0], chosen[1], chosen[2])
+    return bond_matrix
 
 # https://stackoverflow.com/questions/20887555/dead-simple-example-of-using-multiprocessing-queue-pool-and-locking
 # multiprocessing
@@ -391,14 +513,14 @@ def creates_box(m, n):
             if invades_exclusion_space(test_m, i, j, k):
                 test_m[i, j, k] = 1002106110601
                 test_m[i - 2, j + 2, k - 1] = 105710601
-                test_m[i - 4, j + 4, k - 2] = 105710601
-                test_m[i - 6, j + 6, k - 3] = 10571
+                test_m[i - 4, j + 4, k - 2] = 105740602
+                test_m[i - 6, j + 6, k - 3] = 40573
                 test_m[i - 2, j - 2, k + 1] = 105610611
-                test_m[i - 4, j - 4, k + 2] = 105610611
-                test_m[i - 6, j - 6, k + 3] = 10561
+                test_m[i - 4, j - 4, k + 2] = 105640612
+                test_m[i - 6, j - 6, k + 3] = 40563
                 test_m[i, j + 2, k] = 100310021
-                test_m[i, j + 4, k] = 100310021
-                test_m[i, j + 6, k] = 10031
+                test_m[i, j + 4, k] = 100340022
+                test_m[i, j + 6, k] = 40033
                 return True
             else:
                 return False
